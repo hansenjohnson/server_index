3647e2a795386a49bd5fae3d1d846ce0adf9dfc2
Hansen Johnson <hansen.johnson@dal.ca>
2017-09-14 20:24:16 -0300

    try using php

---
+++ index.php
@@ -0,0 +1,86 @@
+$page_title = "Home – My Website";
+$page_description = "Description of this page";
+
+<?php include("header.php"); ?>
+
+		<!-- Banner -->
+			<section id="banner">
+				<div class="inner">
+					<h2>WHaLE Data Visualizer</h2>
+					<em><h5>Interactive tools for displaying oceanographic data</h5></em>
+				</div>
+			</section>
+			
+		<!-- One -->
+			<section id="one" class="wrapper style2">
+				<header class="major">
+					<h2>Applications Currently Online:</h2>
+				</header>
+				<div class="container">
+					<div class="row">
+						<div class="6u">
+							<section class="special">
+								<a href="/live_glider/" class="image fit"><img width="462" height="248" src="images/live_glider.png" alt="" /></a>
+								<h3>Live Glider</h3>
+								<p>Selectively display whale acoustic detections and oceanographic data from live Slocum glider missions</p>
+								<ul class="actions">
+									<li><a href="/live_glider/" class="button alt">Open</a></li>
+								</ul>
+							</section>
+						</div>
+						<div class="6u">
+							<section class="special">
+								<a href="/right_whale_map/" class="image fit"><img width="462" height="248" src="images/right_whale_map.png" alt="" /></a>
+								<h3>Right whale survey map</h3>
+								<p>View the latest right whale sightings and acoustic detections, as well as <em>some</em> measures of survey effort, in Atlantic Canada</p>
+								<ul class="actions">
+									<li><a href="/right_whale_map/" class="button alt">Open</a></li>
+								</ul>
+							</section>
+						</div>
+					</div>
+				</div>
+			</section>
+			
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="container">
+					<div class="row double">
+						<div class="6u">
+							<div class="row collapse-at-2">
+								<div class="6u">
+									<h3>More Information</h3>
+									<ul class="alt">
+										<li><a href="http://dcs.whoi.edu/">robots4whales</a></li>
+										<li><a href="http://gliders.oceantrack.org/">Ocean Tracking Network</a></li>
+										<li><a href="http://meopar.ca/">MEOPAR</a></li>
+										<li><a href="http://apps.cwf-fcf.org/whales/">Canadian Wildlife Federation</a></li>
+										<li><a href="https://github.com/hansenjohnson">App/website source code</a></li>
+									</ul>
+								</div>
+								<div class="6u">
+									<h3>Helpful Resources</h3>
+									<ul class="alt">
+										<li><a href="https://www.windy.com/?44.634,-63.582,5">Weather</a></li>
+										<li><a href="https://www.marinetraffic.com/">Marine Traffic</a></li>
+										<li><a href="https://www.nefsc.noaa.gov/psb/surveys/">Right whale sightings map</a></li>
+										<li><a href="http://www.whalealert.org/">Whale Alert</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="6u">
+							<h2>About</h2>
+							<p>This website, and applications therin, were developed by PhD student Hansen Johnson. Please contact him at hansen(dot)johnson(at)dal.ca with any questions, suggestions, or otherwise.</p>
+						</div>
+					</div>
+					<ul class="copyright">
+						<li>2017 &copy; Hansen Johnson. All rights reserved.</li>
+						<li>Design: <a href="http://templated.co">Based on ION by Templated</a></li>
+						<li>Banner Image: <a href="https://noaateacheratsea.blog/author/odonnellellen/">Allison Henry (NOAA)</a></li>
+					</ul>
+				</div>
+			</footer>
+
+	</body>
+</html>
